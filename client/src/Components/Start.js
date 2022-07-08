c142c9f27d3d04b98278080a240529c625d596ca
Satvik <satvikmakharia@gmail.com>
2022-07-08 11:25:36 +0530

    bug fixes

--- client/src/Components/Start.js
+++ client/src/Components/Start.js
@@ -1,115 +1,107 @@
-import React, { useState } from 'react';
-import './Start.css';
-import axios from 'axios';
-import { useHistory } from 'react-router-dom';
-import logoImg from '../assets/team_work.png';
-import tw1 from '../assets/tw1.png';
+import React, { useState } from "react";
+import "./Start.css";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import logoImg from "../assets/team_work.png";
+import tw1 from "../assets/tw1.png";
 
 export default function Start() {
-	const [roomId, setRoomId] = useState('');
-	const history = useHistory();
-	const handleCreate = async () => {
-		try {
-			console.log(`${process.env.REACT_APP_URL}/createNew`);
-			const res = await axios.get(`${process.env.REACT_APP_URL}/createNew`);
-			setRoomId(res.data.id);
-			history.push(`/rooms/${res.data.id}`);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-	const handleJoin = async (roomId) => {
-		try {
-			const res = await axios.get(`${process.env.REACT_APP_URL}/${roomId}`);
-			console.log(res);
-			if (res.data.isPresent) {
-				setRoomId(res.data.room._id);
-				history.push(`/rooms/${res.data.room._id}`);
-			}
-		} catch (err) {
-			history.push('/PageNotFound');
-		}
-	};
+  const [roomId, setRoomId] = useState("");
+  const history = useHistory();
+  const handleCreate = async () => {
+    try {
+      console.log(`${process.env.REACT_APP_URL}/createNew`);
+      const res = await axios.get(`${process.env.REACT_APP_URL}/createNew`);
+      setRoomId(res.data.id);
+      history.push(`/rooms/${res.data.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleJoin = async (roomId) => {
+    setRoomId(roomId);
+    history.push(`/rooms/${roomId}`);
+  };
 
-	const handleChange = (e) => {
-		let roomId = e.target.value;
-		setRoomId(roomId);
-	};
-	return (
-		<>
-			<div className="start-grid container">
-				<div className="row">
-					<div className="left-col col">
-						<div className="create">
-							<h5>Create Room to have your team workspace</h5>
-							<button
-								type="button"
-								className="button1 btn btn-outline-success"
-								onClick={handleCreate}
-							>
-								Create Room
-							</button>
-						</div>
-						<div className="join">
-							<h5>Join an existing room</h5>
-							<input
-								type="text"
-								value={roomId}
-								onChange={handleChange}
-								placeholder="Enter Room code here..."
-							/>
-							<button
-								type="button"
-								value={roomId}
-								onClick={(e) => handleJoin(e.target.value)}
-								className="button2 btn btn-outline-primary"
-							>
-								Join Room
-							</button>
-						</div>
-					</div>
-					<div className="col">
-						<div
-							id="carouselExampleInterval"
-							className="carousel slide"
-							data-bs-ride="carousel"
-						>
-							<div className="carousel-inner">
-								<div className="carousel-item active" data-bs-interval="7000">
-									<img src={logoImg} className="d-block w-100" alt="..." />
-								</div>
-								<div className="carousel-item" data-bs-interval="7000">
-									<img src={tw1} className="d-block w-100" alt="..." />
-								</div>
-							</div>
-							<button
-								className="carousel-control-prev"
-								type="button"
-								data-bs-target="#carouselExampleInterval"
-								data-bs-slide="prev"
-							>
-								<span
-									className="carousel-control-prev-icon"
-									aria-hidden="true"
-								></span>
-								<span className="visually-hidden">Previous</span>
-							</button>
-							<button
-								className="carousel-control-next"
-								type="button"
-								data-bs-target="#carouselExampleInterval"
-								data-bs-slide="next"
-							>
-								<span
-									className="carousel-control-next-icon"
-									aria-hidden="true"
-								></span>
-								<span className="visually-hidden">Next</span>
-							</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  const handleChange = (e) => {
+    let roomId = e.target.value;
+    setRoomId(roomId);
+  };
+  return (
+    <>
+      <div className="start-grid container">
+        <div className="row">
+          <div className="left-col col">
+            <div className="create">
+              <h5>Create Room to have your team workspace</h5>
+              <button
+                type="button"
+                className="button1 btn btn-outline-success"
+                onClick={handleCreate}
+              >
+                Create Room
+              </button>
+            </div>
+            <div className="join">
+              <h5>Join an existing room</h5>
+              <input
+                type="text"
+                value={roomId}
+                onChange={handleChange}
+                placeholder="Enter Room code here..."
+              />
+              <button
+                type="button"
+                value={roomId}
+                onClick={(e) => handleJoin(e.target.value)}
+                className="button2 btn btn-outline-primary"
+              >
+                Join Room
+              </button>
+            </div>
+          </div>
+          <div className="col">
+            <div
+              id="carouselExampleInterval"
+              className="carousel slide"
+              data-bs-ride="carousel"
+            >
+              <div className="carousel-inner">
+                <div className="carousel-item active" data-bs-interval="7000">
+                  <img src={logoImg} className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item" data-bs-interval="7000">
+                  <img src={tw1} className="d-block w-100" alt="..." />
+                </div>
+              </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleInterval"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleInterval"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- client/src/Components/WorkArea.jsx
+++ client/src/Components/WorkArea.jsx
@@ -1,82 +1,108 @@
-import React, { useState, useEffect } from 'react';
-import Split from 'react-split';
-import Editor from './Editor';
-import User from './User';
-import DoUsername from 'do_username';
-import ActiveUsersModal from './ActiveUsersModal';
+import React, { useState, useEffect, useRef } from "react";
+import Split from "react-split";
+import Editor from "./Editor";
+import User from "./User";
+import DoUsername from "do_username";
+import ActiveUsersModal from "./ActiveUsersModal";
+import axios from "axios";
+import { useParams, useHistory } from "react-router-dom";
 
 const WorkArea = () => {
-	const [orientation, setOrientation] = useState('horizontal');
-	const [name, setName] = useState(DoUsername.generate(15));
-	const [show, setShow] = useState(true);
-	const [activeUsersmodalShow, setActiveUsersModalShow] = useState(false);
-	const [activeUsers, setActiveUsers] = useState([]);
+  const [orientation, setOrientation] = useState("horizontal");
+  const [name, setName] = useState(DoUsername.generate(15));
+  const [show, setShow] = useState(true);
+  const [activeUsersmodalShow, setActiveUsersModalShow] = useState(false);
+  const [activeUsers, setActiveUsers] = useState([]);
+  const [isPresent, setIsPresent] = useState();
+  const data = useRef();
+  let { id } = useParams();
+  const history = useHistory();
 
-	useEffect(() => {
-		let changeOrientation = () => {
-			setOrientation(window.innerWidth < 600 ? 'vertical' : 'horizontal');
-		};
-		changeOrientation();
-		window.onresize = changeOrientation;
-	}, []);
+  useEffect(() => {
+    const checkServer = async (id) => {
+      try {
+        const res = await axios.get(`${process.env.REACT_APP_URL}/${id}`);
+        console.log(res);
+        if (res.data.isPresent) data.current = res.data?.room?.values;
+        setIsPresent(res.data.isPresent);
+      } catch (err) {
+        setIsPresent(false);
+        console.log(err);
+      }
+    };
+    checkServer(id);
 
-	const setUsername = (Uname) => {
-		setName(Uname);
-	};
+    let changeOrientation = () => {
+      setOrientation(window.innerWidth < 600 ? "vertical" : "horizontal");
+    };
+    changeOrientation();
+    window.onresize = changeOrientation;
+  }, [id, history]);
 
-	const setModalShow = (show) => {
-		setShow(show);
-	};
+  useEffect(() => {
+    if (isPresent === false) history.push("/PageNotFound");
+  }, [isPresent, history]);
 
-	const setUsers = (users) => {
-		setActiveUsers(users);
-	};
+  const setUsername = (Uname) => {
+    setName(Uname);
+  };
 
-	return (
-		<div className="">
-			{show && <User setName={setUsername} setShow={setModalShow} />}
-			{!show && (
-				<>
-					<ActiveUsersModal
-						show={activeUsersmodalShow}
-						onHide={() => setActiveUsersModalShow(false)}
-						activeUsers={activeUsers}
-					/>
-					<div className="active-users-btn-container">
-						<button
-							className="btn btn-primary text-end"
-							onClick={() => setActiveUsersModalShow(true)}
-						>
-							Active Users
-						</button>
-					</div>
-					<div className="work-area work">
-						<Split
-							className="wrapper-card"
-							sizes={[50, 50]}
-							minSize={orientation === 'horizontal' ? 300 : 100}
-							expandToMin={true}
-							gutterAlign="center"
-							direction={orientation}
-						>
-							<Editor
-								name={name}
-								id="edit"
-								className="markdown-edit"
-								setUsers={setUsers}
-							/>
-							<div className=" container work_area ">
-								<h2>
-									<b style={{ color: 'white' }}>Here is the Preview</b>
-								</h2>
-								<div id="targetDiv"></div>
-							</div>
-						</Split>
-					</div>
-				</>
-			)}
-		</div>
-	);
+  const setModalShow = (show) => {
+    setShow(show);
+  };
+
+  const setUsers = (users) => {
+    setActiveUsers(users);
+  };
+
+  return (
+    <div className="">
+      {isPresent && show && (
+        <User setName={setUsername} setShow={setModalShow} />
+      )}
+      {isPresent && !show && (
+        <>
+          <ActiveUsersModal
+            show={activeUsersmodalShow}
+            onHide={() => setActiveUsersModalShow(false)}
+            activeUsers={activeUsers}
+          />
+          <div className="active-users-btn-container">
+            <button
+              className="btn btn-primary text-end"
+              onClick={() => setActiveUsersModalShow(true)}
+            >
+              Active Users
+            </button>
+          </div>
+          <div className="work-area work">
+            <Split
+              className="wrapper-card"
+              sizes={[50, 50]}
+              minSize={orientation === "horizontal" ? 300 : 100}
+              expandToMin={true}
+              gutterAlign="center"
+              direction={orientation}
+            >
+              <Editor
+                name={name}
+                id="edit"
+                className="markdown-edit"
+                setUsers={setUsers}
+                isPresent={isPresent}
+              />
+              <div className=" container work_area ">
+                <h2>
+                  <b style={{ color: "white" }}>Here is the Preview</b>
+                </h2>
+                <div id="targetDiv"></div>
+              </div>
+            </Split>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default WorkArea;
